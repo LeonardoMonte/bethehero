b89b77b30aacd05c62cb4cf22aefa82c7dbbab4e
LeonardoMonte <leopk97@hotmail.com>
2020-04-05 04:48:58 -0300

    first class

---
+++ aulas/backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/' , (request,response) => 
+{
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Leonardo Monte'
+    })
+})
+
+
+app.listen(3333)
