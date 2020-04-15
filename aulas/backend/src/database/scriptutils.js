52609555e607dd53ada2a68e5f81252f50e8489f
LeonardoMonte <leopk97@hotmail.com>
2020-04-15 06:12:25 -0300

    Back-end Completo

---
+++ aulas/backend/src/database/scriptutils.js
@@ -0,0 +1,10 @@
+// npm init -y
+// npm install express
+// npx create-react-app frontend
+// npm install nodemon -D
+
+// npm install knex
+// npm install sqlite3
+// npx knex init
+// knex migrate:make nomedaacao
+// knex migrate:latest
